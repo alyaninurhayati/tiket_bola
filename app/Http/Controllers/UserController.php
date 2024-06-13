fb9c6333cad1fc4b5e46fbb5eac2ee9645b54a55
irwan.adhiyanto@gmail.com <irwan.adhiyanto@gmail.com>
2024-06-13 12:53:18 +0700

    saya sudah mengerjakan update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function index(){}
+ 
+    public function create(){}
+
+
+    public function store(request $request){}
+
+
+    public function edit($id){
+        $user = User::findOrFail($id);
+        return view('user.edit', compact('user'));
+    }
+    
+ 
+
+    public function update($id, request $request){
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email,' . $id,
+            'password' => 'nullable|string|min:8|confirmed',
+            
+        ]);
+
+        $user = User::findOrFail($id);
+        $userData = [
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+         
+        ];
+
+        if ($request->filled('password')) {
+            $userData['password'] = Hash::make($request->input('password'));
+        }
+
+        $user->update($userData);
+
+        return redirect()->route('user.edit', $user->id)->with('success', 'User updated successfully.');
+    }
+
+    
+
+
+    public function destroy($id){}
+
+
 }
---
+++ resources/views/user/edit.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Edit User</div>
+
+                <div class="card-body">
+                    @if (session('success'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('success') }}
+                        </div>
+                    @endif
+
+                    <form method="POST" action="{{ route('user.update', $user->id) }}">
+                        @csrf
+                        @method('PUT')
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ $user->name }}" required autocomplete="name" autofocus>
+                            </div>
+                          
+                        </div>
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="text" class="form-control" name="email" value="{{ $user->email }}" required autocomplete="email" autofocus>
+                            </div>
+                          
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Update User
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
