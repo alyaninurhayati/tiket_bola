cd0b1befbf25ee617ab3a004452a6a9fe490d5ae
vickymhmdakbr <vickymuhamad001@gmail.com>
2024-06-13 12:34:46 +0700

    sudah mengerjakan delete

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+    }
+    public function create()
+    {
+    }
+    public function store(Request $request)
+    {
+    }
+    public function edit($id)
+    {
+    }
+    public function update($id, Request $request)
+    {
+    }
+    public function destroy($id)
+    {
+        $User = User::find($id);
+        if (!$User) {
+            return redirect()->back()->with(['Eror', 'Pengguna tidak dapat digunakan'],);
+        }
+
+        $User->delete();
+        return redirect()->route('user.index')->with(['Berhasil', 'Data berhasil dihapus']);
+    }
 }
